491e66eea1c60357097ae15f52ccf3291eec1c60
SHIN BEOM SU <91828379+shin0624@users.noreply.github.com>
2024-11-26 14:49:55 +0900

    2023 KAKAO BLIND RECRUITMENT_개인정보 수집 유효기간

---
+++ level1/2023 KAKAO BLIND RECRUITMENT_개인정보 수집 유효기간/Solution/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Solution {
+    public int[] solution(string today, string[] terms, string[] privacies) {
+        int[] answer = new int[] {};
+        return answer;
+    }
+
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        System.Console.WriteLine(solution.solution());
+    }
+}
